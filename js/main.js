16429f32593d4043419b70cbf29a09cc9c1dfc6a
Adrian <66633552+Adrian-Capbatut@users.noreply.github.com>
2023-06-18 22:41:37 +0300

    new section main

--- js/main.js
+++ js/main.js
@@ -15,3 +15,14 @@ const search = document.querySelector(".search");
 icon.onclick = function () {
     search.classList.toggle("active");
 };
+
+const swipers = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    freeMode: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
